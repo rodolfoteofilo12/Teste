7f70d89ec6577128cea48715bc70706081cd0378
rodolfo <rodolfoteofilo12@hotmail.com>
2017-06-18 13:36:34 -0300

    Primeiro Committ

---
+++ Cidades.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace AlfredoLotar.Livro.AspnetMvc
+{
+    public class Cidades
+    {
+        public int CidadeID { get; set; }
+        public string Nome { get; set; }
+        public string Estado { get; set; }
+    }
+}
+
